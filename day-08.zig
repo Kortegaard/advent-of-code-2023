b3465fd8e4be02bdf71595e25d604d43465c5b84
Anders Kortegaard <ank@askapps.eu>
2023-12-09 15:32:48 +0100

    continued

--- day-08.zig
+++ day-08.zig
@@ -2,24 +2,28 @@ const std = @import("std");
 const utils = @import("./utils.zig");
 const print = std.debug.print;
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
 const Direction = struct {
+    //allocator: std.mem.Allocator,
     left: [3]u8,
     right: [3]u8,
 };
 
 pub fn main() !void {
-    defer _ = gpa.deinit();
+    // Memory is a bit of a pain to destroy xD
+    var ar = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer _ = ar.deinit();
+    var allocator = ar.allocator();
 
     var lineItt = try utils.ReaderFileByLine("./input/day-08-input.txt");
     defer lineItt.deinit();
 
-    var directions: [500]u8 = undefined;
+    var directions: [501]u8 = undefined;
 
     var lineNum: u16 = 0;
-    var directionHM = std.StringHashMap(*Direction).init(gpa.allocator());
+    var directionHM = std.StringHashMap(Direction).init(allocator);
     defer directionHM.deinit();
+    var ghosts = std.ArrayList([]u8).init(allocator);
+
     while (try lineItt.next()) |line| {
         lineNum += 1;
         if (lineNum == 1) {
@@ -29,15 +33,82 @@ pub fn main() !void {
         if (line.len == 0) {
             continue;
         }
-        var d: *Direction = try gpa.allocator().create(Direction);
-        std.mem.copyForwards(u8, &d.left, line[7..10]);
-        std.mem.copyForwards(u8, &d.right, line[12..15]);
-        var ke = [3]u8{ line[0], line[1], line[2] };
-        print("{s}, {s}, {s}\n", .{ line[0..3], line[7..10], line[12..15] });
-        try directionHM.put(&ke, d);
+        var left: [3]u8 = undefined;
+        var right: [3]u8 = undefined;
+        std.mem.copyForwards(u8, &left, line[7..10]);
+        std.mem.copyForwards(u8, &right, line[12..15]);
+        var d = Direction{ .left = left, .right = right };
+
+        var name = try allocator.dupe(u8, line[0..3]);
+        var n2 = try allocator.dupe(u8, line[0..3]);
+        if (n2[2] == 'A') {
+            try ghosts.append(n2);
+        }
+
+        try directionHM.putNoClobber(name, d);
+    }
+
+    var done = false;
+    var numSteps: u32 = 0;
+    //var currLoc = [3]u8{ 'A', 'A', 'A' };
+    //while (!done) {
+    //    var nn: u32 = 0;
+    //    for (directions) |d| {
+    //        var Dir = directionHM.get(&currLoc);
+    //        if (Dir == null) {
+    //            continue;
+    //        }
+    //        nn += 1;
+    //        if (d == 'R') {
+    //            currLoc = Dir.?.right;
+    //        } else if (d == 'L') {
+    //            currLoc = Dir.?.left;
+    //        } else {
+    //            break;
+    //        }
+    //        numSteps += 1;
+    //        if (std.mem.startsWith(u8, &currLoc, "ZZZ")) {
+    //            done = true;
+    //        }
+    //    }
+    //}
+
+    done = false;
+    var numStepsPart2: u32 = 0;
+    var allEndInZ = false;
+    while (!allEndInZ) {
+        var nn: u32 = 0;
+        for (directions) |d| {
+            if (d != 'R' and d != 'L') {
+                break;
+            }
+            allEndInZ = true;
+            numStepsPart2 += 1;
+            if (@mod(numStepsPart2, 10000000) == 0) {
+                print("num: {d}\n", .{numStepsPart2});
+            }
+            for (ghosts.items) |ghost| {
+                var Dir = directionHM.get(ghost);
+                if (Dir == null) {
+                    continue;
+                }
+                nn += 1;
+                if (d == 'R') {
+                    std.mem.copyForwards(u8, ghost, &Dir.?.right);
+                } else if (d == 'L') {
+                    std.mem.copyForwards(u8, ghost, &Dir.?.left);
+                }
+                if (ghost[2] != 'Z') {
+                    allEndInZ = false;
+                }
+            }
+            if (allEndInZ) {
+                break;
+            }
+        }
     }
 
     print("\n", .{});
-    print("Part 1: {d}\n", .{0});
-    print("Part 2: {?}\n", .{0});
+    print("Part 1: {d}\n", .{numSteps});
+    print("Part 2: {?}\n", .{numStepsPart2});
 }
